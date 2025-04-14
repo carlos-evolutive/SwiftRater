3bc24eebeafef204b77c177c4091ace52b64c0f2
Carlos <carlos+git@evolutive.app>
2025-04-15 00:07:13 +0200

    Fixed ios version of ratings made with StoreKit

--- SwiftRater/SwiftRater.swift
+++ SwiftRater/SwiftRater.swift
@@ -508,8 +508,13 @@ import StoreKit
   private func showRatingAlert(host: UIViewController?, force: Bool) {
     NSLog("[SwiftRater] Trying to show review request dialog.")
     if #available(iOS 10.3, *), SwiftRater.useStoreKitIfAvailable, !force {
-      SKStoreReviewController.requestReview()
-      UsageDataManager.shared.isRateDone = true
+        SKStoreReviewController.requestReview()
+        if UsageDataManager.shared.daysBeforeReminding == 0 {
+            UsageDataManager.shared.isRateDone = true
+        } else {
+            // StoreKit automatically manages when ratings can be shown again.
+            UsageDataManager.shared.saveReminderRequestDate()
+        }
     } else {
       let alertController = UIAlertController(title: titleText, message: messageText, preferredStyle: .alert)
       
